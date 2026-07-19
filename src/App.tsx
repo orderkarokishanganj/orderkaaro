872c2774c0876b2f2e8116ce3612a6329654c8b5
orderkarokishanganj <orderkarokishanganj@gmail.com>
2026-07-20 00:30:53 +0530

    Update App.tsx
    Fix: root route loads Home page, catalogue check no longer matches root

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function AppContent() {
       <ScrollToTop />
       {!isCataloguePage && <Header />}
       <Routes>
-        <Route path="/" element={<CataloguePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/catalogue" element={<CataloguePage />} />
         <Route path="/about" element={<AboutPage />} />
